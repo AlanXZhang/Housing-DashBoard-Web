73e58dd88c44f64d5eda9a830dd3b1176f24f4e7
AlanXZhang <72415499+AlanXZhang@users.noreply.github.com>
2022-07-05 00:43:50 -0700

    created skeleton framework

---
+++ hd-web/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.css";
+import AreaStat from "./components/AreaStat";
+
+export default function App() {
+    return (
+        <div className="App">
+            <AreaStat />
+        </div>
+    )
+}
---
+++ hd-web/src/components/AreaStat.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+export default function AreaStat() {
+
+    // Create an event listener that will update the state when the zooms in or out of the map or drag the map.
+    // Maybe create a removeEventListener function that will remove the event listener when the component is unmounted.
+
+    return (
+        <div className="area-stats">
+            <h3>Area Overview</h3>
+        </div>
+    )
+}
